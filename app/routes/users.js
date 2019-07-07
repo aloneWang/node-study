8326ceef35823a69aa943c2ea62ac52196f26bb3
wangjianping <alone0228@126.com>
2019-07-08 00:56:01 +0800

    调用 koa-jwt 处理路由权限验证

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,7 @@
 
 const Router = require('koa-router')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const router = new Router({prefix:'/users'})
 
 const { secret }  = require('../config.js')
@@ -9,16 +10,18 @@ const {findById, create, delete: del, getAll, login} = require('../controllers/u
 
 // 添加路由中间件 token 验证
 
-const auth = async (ctx, next) => {
-  const { authorization = '' } = ctx.request.header;
-  const token = authorization.replace('Bearer','')
-  try {
-    jwt.verify(token, secret)
-  } catch (err) {
-    ctx.throw(401, err.message)
-  }
-  await next()
-}
+// const auth = async (ctx, next) => {
+//   const { authorization = '' } = ctx.request.header;
+//   const token = authorization.replace('Bearer','')
+//   try {
+//     jwt.verify(token, secret)
+//   } catch (err) {
+//     ctx.throw(401, err.message)
+//   }
+//   await next()
+// }
+const auth = jwt({secret})
+
 router.get('/',getAll)
 router.get('/:id',findById)
 
